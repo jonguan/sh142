c7b691d8a4b3b718b0cf7a860f366e93a3c051e5
Øyvind Tangen <oyvindrtangen@gmail.com>
2011-10-25 17:42:12 -0700

    Project

---
+++ sh142/sh142/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  sh142
+//
+//  Created by Øyvind Tangen on 25.10.11.
+//  Copyright 2011 Universitetet i Oslo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
